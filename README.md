62d283ef3b63239726484263863c469684a6f1d4
Primoz Cigler <primoz.cigler@gmail.com>
2013-10-06 13:06:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Flickr Gallery WP Widget
+=====================
+
+This is very basic WP widget which I use in my themes to display the latest photos from the widget. It is both very simple to use (which means you can easily add new features) and lightweight.
+
+It doesn't come with any CSS styling, just 2 JavaScript files are loaded and there are just 2 options:
+
+*	which ID to fetch the images from and
+*	how many of the images to display (1 to 10)
+
+Installation
+------------
+
+Clone this repository (or download zipball and unzip it) to your plugins directory (usually `wp-content/plugins`) and activate it over the wp-admin.
+
+Customizations
+--------------
+
+In the file `js/custom.js` you will find the jQuery plugin `jflickrfeed` called. You can customize the output markup and customize the plugin by your needs, the detailed instructions and the demo can be found [on this original post by Joel Sutherland](http://www.gethifi.com/blog/a-jquery-flickr-feed-plugin).
+
+Credits
+-------
+
+* JavaScript library created by [Joel Sutherland](http://www.gethifi.com/blog/a-jquery-flickr-feed-plugin)
+
+Created By
+----------
+
+[@primozcigler](http://twitter.com/primozcigler) from [ProteusThemes](http://www.proteusthemes.com/)
---
+++ proteusthemes-flickr-gallery-widget.php
@@ -0,0 +1,126 @@
+<?php 
+/**
+ * Plugin Name: Flickr Gallery Widget
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Displays a simple gallery from Flickr
+ * Version: 1.0.0
+ * Author: Primož Cigler, @primozcigler
+ * Author URI: http://twitter.com/primozcigler
+ * License: GPL2
+ * @todo add the URL above
+ */
+
+/**************************************
+ * Flickr gallery
+ * -----------------------------------
+ * Last images from the flickr account 
+ **************************************/
+
+class ProteusThemes_Flickr_Gallery_Widget extends WP_Widget {
+	
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		false, // ID, auto generate when false
+			__( "ProteusThemes Flickr Gallery" ), // Name
+			array(
+				'description' => __( 'Gallery of the latest 9 images from any Flickr account' ),
+			)
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		?>
+		
+            <?php echo $args['before_title'] . $instance['title'] . $args['after_title']; ?>
+
+            <div class="sidebar-post sidebar-gallery">
+                <div class="js-jflickrfeed-fetch flickr" data-userid="<?php echo $instance['user_id']; ?>" data-number="<?php echo $instance['number']; ?>">
+                    <!-- gallery loaded via JavaScript goes here -->
+                </div>
+            </div>	
+		
+		<?php
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['user_id'] = strip_tags( $new_instance['user_id'] );
+		$instance['number'] = min( (int) $new_instance['number'], 10 );
+
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+            
+        $title = isset( $instance['title'] ) ? $instance['title'] : __( 'Flickr Gallery' );
+        $user_id = isset( $instance['user_id'] ) ? $instance['user_id'] : '52617155@N08';
+        $number = isset( $instance['number'] ) ? $instance['number'] : 9;
+        
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'user_id' ); ?>"><?php _e( 'User ID:' ); ?></label> 
+			<input id="<?php echo $this->get_field_id( 'user_id' ); ?>" name="<?php echo $this->get_field_name( 'user_id' ); ?>" type="text" value="<?php echo esc_attr( $user_id ); ?>" />
+			<br />
+			<small><a href="https://www.diigo.com/item/image/3rli1/h0a2?size=o" target="_blank">Where do you get this User ID?</a></small>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of images (limited to 10):' ); ?></label> 
+			<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" value="<?php echo (int) $number; ?>" />
+		</p>
+		
+		<?php 
+	}
+
+} // class Home_Testimonials
+add_action( 'widgets_init', create_function( '', 'register_widget( "ProteusThemes_Flickr_Gallery_Widget" );' ) );
+
+
+
+if ( ! function_exists( "enqueue_flickr_gallery_deps" ) ) {
+	/**
+	 * Adds the jQuery plugin and the basic CSS
+	 * @return void
+	 */
+	function enqueue_flickr_gallery_deps() {
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'flickr-js', plugin_dir_url( __FILE__ ) . "js/jflickrfeed/jflickrfeed.min.js", array( 'jquery' ), FALSE, TRUE );
+		wp_enqueue_script( 'flickr-js-custom', plugin_dir_url( __FILE__ ) . "js/custom.js", array( 'flickr-js' ), FALSE, TRUE );
+	}
+	add_action( 'wp_enqueue_scripts', 'enqueue_flickr_gallery_deps' );
+}
